b1e95d2f282a23ae73460a590cb127762f6de88f
Shridhar Naik <shridharnaikmirjan1@gmail.com>
2026-05-18 13:53:07 +0530

    Designed a box

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="container">
+        <header className="header"><h2>Header</h2></header>
+        <div className="middle">
+          <div className="left">
+            <div className="hero"><h2>Hero</h2></div>
+            <div className="sidebar"><h2>Sidebar</h2></div>
+          </div>
+          <div className="right">
+            <div className="main-content"><h2>Main Content</h2></div>
+            <div className="extra-content"><h2>Extra content</h2></div>
+          </div>
+        </div>
+        <div className="last">
+          <div className="last-left"><h2>Realated Images</h2></div>
+          <div className="last-right"><h2>Related Posts</h2></div>
+        </div>
+        <footer className="footer"><h2>Footer</h2></footer>
+      </div>
+    </>
+  )
+}
+
+export default App
